7fdb2b6087a67c2d815fee058a8a107ea2a53582
jchu91638 <jchu91638@gmail.com>
2021-11-30 20:39:39 +0630

    data updated

---
+++ testing.js
@@ -0,0 +1,40 @@
+const root = document.querySelector('.root');
+
+async function getData(){
+    const response = await fetch('domatio-data.json');
+    const data = await response.json();
+    return data;
+}
+
+getData().then(data => {
+
+    
+
+    const find= (value)=> {
+        const filterObj = data.domatio.filter(item => item.tags.indexOf(value) !== -1);
+        console.log(filterObj);
+        
+        data.domatio.map(item => {
+
+            root.innerHTML += `
+            <div>
+                <h1>${item.name}</h1>
+                <p>
+                    ${
+                        item.title === undefined ? "Hello": item.title
+                    }
+                </p>
+                ${
+                    item.image ===  undefined ? 
+                     `<h3>Coming Soon</h3>` : `<img src=${item.image} alt="${item.name} image" />` 
+                }
+                
+            </div>
+        `;
+        })
+    }
+    find("free");
+
+    
+    
+});
